c16f6ddbdab6077733d4e32ff040e2fb23815d02
Glitch (evergreen-pike) <none>
2019-12-18 21:14:14 +0000

    👏💐 Checkpoint ./package.json:9182067/34 ./server.js:9182067/319

--- server.js
+++ server.js
@@ -2,13 +2,22 @@
 
 var express = require('express');
 var cors = require('cors');
+const bodyParser = require('body-parser');
 
 // require and use "multer"...
 
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use('/public', express.static(process.cwd() + '/public'));
+
+app.route("/api/fileanalyse").post((req, res) => {
+  res.send(req);
+})
+
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
